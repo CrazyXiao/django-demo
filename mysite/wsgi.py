d1e4b6c992fa442b00de63d445b82668b40273b6
CrazyXiao <1083734876@qq.com>
2019-02-28 21:03:02 +0800

    apache配置

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
